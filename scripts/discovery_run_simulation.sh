067ea58597e704eb6e69867c434448c2c8011288
Julian Zucker <julian.zucker@gmail.com>
2019-06-04 09:00:33 -0400

    Run in 1000x parallel, add deploy script
    Authored-by: Julian Zucker <julian.zucker@gmail.com>

---
+++ scripts/discovery_run_simulation.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=EGTSimulations
+#SBATCH --nodes=100
+#SBATCH --tasks-per-node=10
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8Gb
+#SBATCH --partition=general
+
+module load oracle_java/jdk1.8.0_181
+
+srun egt_simulations.sh
---
+++ scripts/egt_simulations.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Args: #Runs, P1 proportion, Q1 proportion, D(isagreement point), <strategies>
+
+exec java -jar ./egt.jar 10 .9 .9 1 1 3 5 7 9
