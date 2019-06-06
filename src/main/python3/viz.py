f69d71e14014708e3b0c6d85e7d2bdf2a2ca65d4
Daniel Rassaby <drassaby@gmail.com>
2019-06-06 01:25:35 -0400

    Minor bug fixes and documentation
    * Ran on our current discovery data

--- src/main/python3/viz.py
+++ src/main/python3/viz.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 
-import csv
-from typing import Pattern, List
-
+from typing import Pattern
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
 import argparse
 import os
 import re
 
 
-# output_filename = sys.argv[1]
-
-
 def main(args: argparse.Namespace):
-    input_location: str = args.input
-    output_directory: str = args.output_directory
+    input_location: str = args.input[:-1] if len(args.input) > 1 and args.input.endswith("/") else args.input
+    output_directory: str = \
+        args.output_directory[:-1]\
+            if len(args.output_directory) > 1 and args.output_directory.endswith("/") \
+            else args.output_directory
     if not os.path.isdir(output_directory):
         raise NotADirectoryError(f"{output_directory} is not a valid output directory,"
                                  " see usage with:\n    ./viz.py --help")
 
     if os.path.isdir(input_location):
-        input_files = [f"{input_location}/{filename} " for filename in
+        input_files = [f"{input_location}/{filename}" for filename in
                        filter(lambda file: file.endswith(".out"), os.listdir(input_location))]
     elif os.path.isfile(input_location):
         input_files = [input_location]
@@ -30,9 +27,12 @@ def main(args: argparse.Namespace):
         raise FileNotFoundError(f"{input_location} is not a valid output directory,"
                                 " see usage with:\n    ./viz.py --help")
 
+    print("Creating heatmap for:")
     for input_file in input_files:
-        egt_result: EGTResult = EGTResult(f"{input_file}")
+        print(f"    {input_file}")
+        egt_result: EGTResult = EGTResult(input_file)
         egt_result.put_heatmap(output_directory)
+    print(f"\nDONE:\nFind your result in {output_directory}")
 
 
 class EGTResult:
@@ -42,14 +42,13 @@ class EGTResult:
 
     def __init__(self, input_file):
         """
-        Input Files
-        :param input_file:
+        :param input_file: The relative or absolute path to the file containing the data we need to parse.
         """
         self.input_file = input_file
         problem_type_re: Pattern[str] = re.compile("P1=(\d+\.\d+),"
                                                    " Q1=(\d+\.\d+),"
                                                    " D=(\d+\.\d+), "
-                                                   "strategies=Vector\(((?:\d+\.\d+, )+\d+.\d)\), "
+                                                   "strategies=Vector\(((?:\d+\.\d+, )+\d+\.\d)\), "
                                                    "simulation=(Minimal|Moderate)")
         with open(input_file) as input_fd:
             input_data: str = input_fd.read()
@@ -69,6 +68,9 @@ class EGTResult:
         self.result_matrix = self.find_parsed_result()
 
     def find_parsed_result(self) -> np.array:
+        """
+        :return: The combined matrix from all the experiments documented in our input file
+        """
 
         line_re: Pattern[str] = re.compile("(?:(\d+\.\d+) )+(\d+\.\d+)\n")
         with open(self.input_file) as input_fd:
@@ -89,15 +91,16 @@ class EGTResult:
         return matrix
 
     def put_heatmap(self, at: str) -> None:
-        """ TODO: Actually write the png files to some location
-        :param at: Where to write the png files to
+        """
+        Creates heatmaps as pngs at the given location.
+        :param at: Where to write the png file to
         """
         labels = [str(strategy) for strategy in self.strategies]
         title = "Proportion of (P1, Q1) playing strategies in P and Q arenas with " \
             f"P1={self.p1}, Q1={self.q1}, D={self.d}"
 
         fig, ax = plt.subplots()
-        # im = ax.imshow(self.result_matrix)
+        ax.imshow(self.result_matrix)
 
         ax.set_xticks(np.arange(self.result_matrix.shape[0]))
         ax.set_yticks(np.arange(self.result_matrix.shape[0]))
@@ -123,7 +126,9 @@ class EGTResult:
 
         plt.title(title)
         plt.colorbar(heatmap)
-        plt.show()
+        # plt.show()
+        # The name of the figure is the name of the parsed data file, and can be found at location at.
+        plt.savefig(f"{at}/{self.input_file.split('/')[-1].split('.')[0]}.png")
 
 
 if __name__ == '__main__':
